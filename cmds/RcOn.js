c1c84f31bb7a0d783cd9eba5dba901ba92f6b193
ExoOnthesky <77278013+ExoOnthesky@users.noreply.github.com>
2021-01-13 09:49:12 +1100

    Update RcOn.js

--- cmds/RcOn.js
+++ cmds/RcOn.js
@@ -5,8 +5,7 @@ module.exports.run = async (bot,message,args) => {
     let RcEmbed = new Discord.MessageEmbed()
     .setColor("#0083FF")
     .setTitle('__**Recrutements staff - Air Publicity**__')
-    .addField('Air Publicity :airplane_departure: est heureux de vous annoncer que les recrutements staff 👮‍♂️ sont ouvert !')
-    .addField('__**Rendez-vous dans #【👮‍♂️】recrutements !**__');
+    .addField('Air Publicity :airplane_departure: est heureux de vous annoncer que les recrutements staff 👮‍♂️ sont ouvert !', '__**Rendez-vous dans #【👮‍♂️】recrutements !**__')
     .setFooter('Air publicity - Bot')
 
     message.channel.send(RcEmbed)
